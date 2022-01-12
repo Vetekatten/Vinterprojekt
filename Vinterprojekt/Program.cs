efbfc072dd4ea47da68419b7e1e7040bcf9535a3
Joen Wilund <vetekatten3@gmail.com>
2022-01-12 12:43:48 +0100

    första commit

---
+++ Vinterprojekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vinterprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
